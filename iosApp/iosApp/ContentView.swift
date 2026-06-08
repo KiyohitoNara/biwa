d8099e3290f214c77ebef30d6cae6de41bc608d4
KiyohitoNara <kiyohitonara1216@gmail.com>
2026-06-09 07:59:39 +0900

    Wire up SwiftUI NavigationStack as root navigation

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -1,17 +1,37 @@
 import SwiftUI
-import ComposeApp
 
-struct ContentView: View {
-    var body: some View {
-        ComposeView()
-            .ignoresSafeArea(.all)
-    }
+private enum AppRoute: Hashable {
+    case mediaViewer(String)
+    case tagManagement
+    case settings
+    case about
 }
 
-struct ComposeView: UIViewControllerRepresentable {
-    func makeUIViewController(context: Context) -> UIViewController {
-        MainViewControllerKt.MainViewController()
-    }
+struct ContentView: View {
+    @State private var path = NavigationPath()
 
-    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {}
+    var body: some View {
+        NavigationStack(path: $path) {
+            LibraryView(
+                onOpenMediaViewer: { id in path.append(AppRoute.mediaViewer(id)) },
+                onManageTags: { path.append(AppRoute.tagManagement) },
+                onOpenSettings: { path.append(AppRoute.settings) }
+            )
+            .navigationDestination(for: AppRoute.self) { route in
+                switch route {
+                case .mediaViewer(let id):
+                    MediaViewerView(mediaId: id, onBack: { path.removeLast() })
+                case .tagManagement:
+                    TagManagementView(onBack: { path.removeLast() })
+                case .settings:
+                    SettingsView(
+                        onBack: { path.removeLast() },
+                        onAbout: { path.append(AppRoute.about) }
+                    )
+                case .about:
+                    AboutView()
+                }
+            }
+        }
+    }
 }
